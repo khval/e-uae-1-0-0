e67948205c795d5b39462df65d45018634483108
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2025-10-26 01:23:09 +0200

    Remove obsolete warnings on AmigaOS4.

--- src/sd-amigaos/sound.h
+++ src/sd-amigaos/sound.h
@@ -18,6 +18,17 @@
 #include <proto/dos.h>
 #include <clib/alib_protos.h>
 
+#ifdef __amigaos4__
+#undef CreateMsgPort
+#undef DeleteMsgPort
+#undef CreateIORequest
+#undef DeleteIORequest
+#define CreateIORequest(io,size) AllocSysObjectTags(ASOT_IOREQUEST,io, ASOIOR_Size,size,TAG_END)
+#define DeleteIORequest(io) FreeSysObject(ASOT_IOREQUEST,io)
+#define CreateMsgPort() AllocSysObjectTags(ASOT_PORT,TAG_END)
+#define DeleteMsgPort(p) FreeSysObject(ASOT_PORT,p)
+#endif
+
 extern struct AHIRequest *AHIio[];
 extern struct AHIRequest *linkio;
 
@@ -29,25 +40,26 @@ extern int sndbufsize;
 STATIC_INLINE void flush_sound_buffer (void)
 {
 	void *tmp;
+	struct AHIRequest *io = AHIio[bufidx];
 
-	AHIio[bufidx]->ahir_Std.io_Message.mn_Node.ln_Pri = 0;
-	AHIio[bufidx]->ahir_Std.io_Command  = CMD_WRITE;
-	AHIio[bufidx]->ahir_Std.io_Data     = buffers[bufidx];
-	AHIio[bufidx]->ahir_Std.io_Length   = sndbufsize;
-	AHIio[bufidx]->ahir_Std.io_Offset   = 0;
-	AHIio[bufidx]->ahir_Frequency       = currprefs.sound_freq;
+	io->ahir_Std.io_Message.mn_Node.ln_Pri = 0;
+	io->ahir_Std.io_Command  = CMD_WRITE;
+	io->ahir_Std.io_Data     = buffers[bufidx];
+	io->ahir_Std.io_Length   = sndbufsize;
+	io->ahir_Std.io_Offset   = 0;
+	io->ahir_Frequency       = currprefs.sound_freq;
 	if (currprefs.sound_stereo)
-		AHIio[bufidx]->ahir_Type    = AHIST_S16S;
+		io->ahir_Type    = AHIST_S16S;
 	else
-		AHIio[bufidx]->ahir_Type    = AHIST_M16S;
-	AHIio[bufidx]->ahir_Volume          = 0x10000;          /* Full volume */
-	AHIio[bufidx]->ahir_Position        = 0x8000;           /* Centered */
-	AHIio[bufidx]->ahir_Link            = linkio;
-	SendIO ((struct IORequest *) AHIio[bufidx]);
+		io->ahir_Type    = AHIST_M16S;
+	io->ahir_Volume          = 0x10000;          /* Full volume */
+	io->ahir_Position        = 0x8000;           /* Centered */
+	io->ahir_Link            = linkio;
+	SendIO( (struct IORequest *) io );
 
 	if (linkio)
 	    WaitIO ((struct IORequest *) linkio);
-	linkio = AHIio[bufidx];
+	linkio = io;
 	/* double buffering */
 	bufidx = 1 - bufidx;
 	sndbuffer = sndbufpt = (uae_u16*) buffers[bufidx];
